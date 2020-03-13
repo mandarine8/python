78f4e12d6a0e10cb50f78c331d52b333e34c0340
Amandine Car <caramandine@gmail.com>
2020-03-13 17:10:36 -0400

    beginning of an exercise

---
+++ vowels.py
@@ -0,0 +1,6 @@
+# fonction pour calculer le nombre de voyelles dans un mot
+# definir une fonction get_vowels_numbers(mot)
+# créer un compteur de voyelles
+# pour chaque lettre du mot, verifier si il s'agit d'une voyelle
+# si c'est le cas, ajouter 1 au compteur
+# a la fin de la fonction, renvoyer le compteur
